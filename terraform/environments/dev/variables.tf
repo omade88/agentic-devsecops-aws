f6514350dfa3e2888fb2be7e4da80a6ef4155f56
cloud <omadetech@gmail.com>
2026-01-16 19:50:23 -0500

    Add all the missing variables that are used in main.tf and defined in terraform.tfvars

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -1,23 +1,52 @@
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "public_subnet_count" {
+  description = "Number of public subnets"
+  type        = number
+  default     = 2
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["us-west-1a", "us-west-1c"]
+}
+
 variable "instance_type" {
   description = "The type of EC2 instance to launch"
-  default     = "t2.micro"
+  type        = string
+  default     = "t3.micro"
 }
 
 variable "ami_id" {
   description = "The AMI ID to use for the EC2 instance"
-  default     = "ami-0c55b159cbfafe1f0"  # Example AMI ID, replace with a valid one for your region
-}
-
-variable "vpc_id" {
-  description = "The ID of the VPC where the EC2 instance will be deployed"
-}
-
-variable "subnet_id" {
-  description = "The ID of the subnet where the EC2 instance will be deployed"
+  type        = string
 }
 
 variable "allowed_ip_ranges" {
   description = "The IP ranges that are allowed to access the EC2 instance"
   type        = list(string)
-  default     = ["0.0.0.0/0"]  # Replace with more restrictive ranges as needed
 }
