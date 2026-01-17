befb31714f08f0e82251e6b2447651e8be66c0d2
cloud <omadetech@gmail.com>
2026-01-17 00:56:27 -0500

    fix: Update Lambda module to use AWS provider v6.x

--- terraform/modules/lambda-functions/main.tf
+++ terraform/modules/lambda-functions/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"  # Updated to match dev environment
     }
     archive = {
       source  = "hashicorp/archive"
